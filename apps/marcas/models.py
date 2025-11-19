068b866535d7c87faced9e313dffcb414747fa04
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-19 20:27:41 -0300

    Adições no models

--- apps/marcas/models.py
+++ apps/marcas/models.py
@@ -10,3 +10,17 @@ class Marca(models.Model):
     ativo = models.BooleanField('Ativo', default=True)
     site_oficial = models.URLField('Site Oficial', max_length=200)
     logo_url = models.URLField('Logo URL', max_length=300)
+
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+    def consultar_site(self):
+        return self.site_oficial
+
+    def verificar_ativo(self):
+        return "Marca ativa" if self.ativo else "Marca inativa"
