5636c0d18defcb2047503761eb2487ff581b553b
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 03:16:10 -0300

    Adiçoes serializer

--- apps/motoristas/serializer.py
+++ apps/motoristas/serializer.py
@@ -1,2 +1,17 @@
 from rest_framework import serializers
 from .models import Motorista
+
+class MotoristaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Motorista
+        fields = [
+            'id',
+            'nome',
+            'cnh',
+            'data_validade_cnh',
+            'categoria_cnh',
+            'data_nascimento',
+            'telefone',
+            'email',
+            'autenticado',
+        ]
