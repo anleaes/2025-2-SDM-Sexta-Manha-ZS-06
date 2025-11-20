cac1aff28a039249daf6592ddb6cf64a46f5a01c
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 02:59:56 -0300

    Adiçoes urls

--- apps/modopagamentos/urls.py
+++ apps/modopagamentos/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+
+app_name = 'cliente'
+
+router = routers.DefaultRouter()
+router.register('', views.ClienteViewSet, basename='Clientes')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
