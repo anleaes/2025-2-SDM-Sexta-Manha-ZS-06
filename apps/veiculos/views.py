9cb4d88b9ba07a0c4ac444300c90703da34c3e6e
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 05:35:34 -0300

    Adiçoes veiculos

--- apps/veiculos/views.py
+++ apps/veiculos/views.py
@@ -4,3 +4,7 @@ from .models import Veiculo
 from .serializer import VeiculoSerializer
 
 # Create your views here.
+
+class VeiculoViewSet(viewsets.ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
