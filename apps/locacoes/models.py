3b0a0d7d8978d36e645b9df2660bbc9af1799c5a
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 05:50:54 -0300

    Criação locacoes

--- apps/locacoes/models.py
+++ apps/locacoes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from veiculos.models import Veiculo
+from clientes.models import Cliente
+from modopagamentos.models import ModoPagamento
+from motoristas.models import Motorista
 
 # Create your models here.
+
