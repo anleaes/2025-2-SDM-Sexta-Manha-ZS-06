4cbc87ed7fa8e0029c869f00024042b5a9e7b55a
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 02:06:40 -0300

    Adiçoes serializer

--- apps/clientes/serializer.py
+++ apps/clientes/serializer.py
@@ -1,2 +1,16 @@
 from rest_framework import serializers
 from .models import Cliente
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = [
+            'id',
+            'nome',
+            'cpf',
+            'data_nascimento',
+            'endereco',
+            'telefone',
+            'email',
+            'ativo',
+        ]
