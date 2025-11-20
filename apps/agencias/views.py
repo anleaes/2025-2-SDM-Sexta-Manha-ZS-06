58cc70f7a68c7aaa4886005bdc0254bba0cab6f6
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 05:16:43 -0300

    Adiçoes agencias

--- apps/agencias/views.py
+++ apps/agencias/views.py
@@ -4,3 +4,7 @@ from rest_framework import viewsets
 from .serializer import AgenciaSerializer
 
 # Create your views here.
+
+class AgenciaViewSet(viewsets.ModelViewSet):
+    queryset = Agencia.objects.all()
+    serializer_class = AgenciaSerializer
