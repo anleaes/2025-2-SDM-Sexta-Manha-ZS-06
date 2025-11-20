0659845026cd1a54ced474e5d0be290f42f6540c
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 03:47:37 -0300

    Adiçoes modelos

--- apps/modelos/views.py
+++ apps/modelos/views.py
@@ -4,3 +4,7 @@ from .models import Modelo
 from .serializer import ModeloSerializer
 
 # Create your views here.
+
+class ModeloViewSet(viewsets.ModelViewSet):
+    queryset = Modelo.objects.all()
+    serializer_class = ModeloSerializer
