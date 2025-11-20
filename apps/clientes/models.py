a98cadbb134356f4f29855317fc679b8d43e192b
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 00:18:45 -0300

    criação inicial models

---
+++ apps/clientes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=150)
+    cpf = models.CharField('CPF', max_length=14, unique=True)
+    data_nascimento = models.DateField('Data de Nascimento')
+    endereco = models.CharField('Endereço', max_length=255)
+    telefone = models.CharField('Telefone', max_length=20)
+    email = models.EmailField('E-mail', unique=True)
+    ativo = models.BooleanField('Ativo', default=True)
