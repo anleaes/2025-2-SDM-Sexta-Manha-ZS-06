e8557650ceb0a0ce64dd63d04a2907135165b4d6
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 06:01:13 -0300

    Adiçoes views

--- apps/locacoes/views.py
+++ apps/locacoes/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Locacao
 from .serializer import LocacaoSerializer
+
 # Create your views here.
+
+class LocacaoViewSet(viewsets.ModelViewSet):
+    queryset = Locacao.objects.all()
+    serializer_class = LocacaoSerializer
