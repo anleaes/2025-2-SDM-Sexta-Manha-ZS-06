75a405083b3b6a871c6168b8833f7074e8f1c2dd
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 02:51:02 -0300

    criação modopagamentos

--- apps/modopagamentos/models.py
+++ apps/modopagamentos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ModoPagamento(models.Model):
+    STATUS_PAGAMENTO_CHOICES = [
+        ('pendente', 'Pendente'),
+        ('pago', 'Pago'),
+        ('recusado', 'Recusado'),
+        ('estornado', 'Estornado'),
+    ]
