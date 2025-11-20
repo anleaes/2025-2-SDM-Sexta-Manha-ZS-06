e4ddba52571a0cced090cd9a048cf8a9d9dfc04c
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 03:12:15 -0300

    Criação models + verbose name

--- apps/motoristas/apps.py
+++ apps/motoristas/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MotoristasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'motoristas'
+    verbose_name = 'Motoristas'
