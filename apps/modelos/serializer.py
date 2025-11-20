d62e73029b7c9d75779ae87648b48e2da5ec2fb0
GuilhermeRodrigues05 <guilherme.rodrigues.959595@gmail.com>
2025-11-20 03:41:29 -0300

    Adiçoes serializer

--- apps/modelos/serializer.py
+++ apps/modelos/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Modelo
+
+class ModeloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Modelo
+        fields = '__all__'
